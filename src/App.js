128d1ba6bdb0bae5065f72b23bcf5166ed785679
praveen <upparipraveen2003@gmail.com>
2023-07-18 17:27:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Account from './Account'
+import Form from './Form'
+
+const App = () => {
+  return (
+    <div className='center'>
+      
+    <Form />
+    <Account />
+    </div>
+  )
+}
+
+export default App
